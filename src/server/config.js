b15c2561e555ed358f5a625aaedd7a28ef461eab
linkin <pablogermanjimenez7@gmail.com>
2025-12-08 14:03:17 -0300

    clase Server configurada en config.js

---
+++ src/server/config.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import {dirname} from 'path';   
+import {fileURLToPath} from 'url';
+
+export default class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT || 3001;
+        this.middlewares();
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        console.log(__dirname)
+        this.app.use(express.static(__dirname + '/public'));
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{ console.info(`Servidor ejecutándose en el puerto ${this.port}`)});
+    }
+}
+
+
+
+
