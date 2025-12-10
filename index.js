f1ae50c6040774181e053ee3c65fb839efdedf84
linkin <pablogermanjimenez7@gmail.com>
2025-12-10 17:47:47 -0300

    eliminacion de datos dbConfig y pull a dev remoto

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import router from "./src/routes/index.routes.js";
 import Server from "./src/server/config.js";
 
 const server = new Server();
 
+server.app.use('api', router);
+
 server.listen();
+
