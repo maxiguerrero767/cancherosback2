bdb31ff47a8a4aaa1a94b7165bbc0b30e8ee3fb1
Maximiliano <maximilianoguerreromeriles@gmail.com>
2025-12-18 18:00:43 -0400

    update

--- src/routes/reservas.route.js
+++ src/routes/reservas.route.js
@@ -5,10 +5,9 @@ import {
     updateReserva,
     deleteReserva
 } from '../controllers/reservas.controller.js'
-import { validarJWT } from '../helpers/validarJWT.js';
 const router = Router()
 
-router.get('/', validarJWT, getReservas);
+router.get('/', getReservas);
 router.post('/', createReserva)
 router.put('/:id', updateReserva)
 router.delete('/:id', deleteReserva)
