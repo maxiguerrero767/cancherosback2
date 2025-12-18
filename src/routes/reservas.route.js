edc5f36b43e94a13d9eb4cf196235aa50db8afd9
Maximiliano <maximilianoguerreromeriles@gmail.com>
2025-12-18 17:57:33 -0400

    update

--- src/routes/reservas.route.js
+++ src/routes/reservas.route.js
@@ -1,26 +1,16 @@
 import { Router } from 'express'
-import { check } from 'express-validator';
-import { validarResultado } from '../helpers/validarCampos.js';
-import { validarJWT } from '../helpers/validarJWT.js';
 import {
     getReservas,
     createReserva,
     updateReserva,
     deleteReserva
 } from '../controllers/reservas.controller.js'
-
+import { validarJWT } from '../helpers/validarJWT.js';
 const router = Router()
 
-router.get('/', getReservas);
+router.get('/', validarJWT, getReservas);
 router.post('/', createReserva)
-router.put('/:id', 
-    [
-        validarJWT, 
-        check('id', 'No es un ID válido').isMongoId(),
-        validarResultado
-    ],
-    updateReserva
-);
-router.delete('/:id', validarJWT, deleteReserva);
+router.put('/:id', updateReserva)
+router.delete('/:id', deleteReserva)
 
 export default router
